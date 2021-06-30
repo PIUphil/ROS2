3fccf7bcf56d748603f4e59da00031ccb2c10e6b
PIUphil <ahnkp08@daum.net>
2021-06-30 10:29:41 +0900

    bind

--- bind.cpp
+++ bind.cpp
@@ -9,8 +9,32 @@ auto func = std::bind(sum, std::placeholder::_1, std::placeholder::_2);
 
 int re = func(10, 20);
 
-
 /*
 10이 a로 들어가고
 20이 b로 들어감
 */
+
+auto func2 = std::bind(sum, 10, std::placeholder::_1);
+int re2 = func(20);
+
+
+////////////////////////////////////////////////////////////
+
+class A {
+public:
+	int sum(int a, int b) {
+		return a+b;
+	}
+	
+	void bindFunc(std::function<int(int, int)>& func) {
+		func = std::bind(&A::sum, this, std::placeholder::_1, std::placeholder::_2);	
+	}
+}
+
+A a;
+
+auto func;		// std::function<int(int, int)> func;
+a.bindFunc(func);
+func(10, 20);
+
+
