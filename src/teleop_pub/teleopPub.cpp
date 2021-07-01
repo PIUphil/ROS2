5a8e571c91ed83752ba40080dec6ef86e86422c3
PIUphil <ahnkp08@daum.net>
2021-07-01 15:50:15 +0900

    Update teleopPub.cpp

--- src/teleop_pub/teleopPub.cpp
+++ src/teleop_pub/teleopPub.cpp
@@ -1,10 +1,10 @@
-#include <chrono>
+// #include <chrono>
 #include <functional>
-#include "teleopPub.h"
+#include "TeleopPub.h"
 
-using namespace std::chrono_literals;
+// using namespace std::chrono_literals;
 
-void teleopPub::initMsg()
+void TeleopPub::init_msg()
 {
     this->msg_.linear.x = 0.0;
     this->msg_.linear.y = 0.0;
@@ -16,62 +16,62 @@ void teleopPub::initMsg()
 }
 
 
-void teleopPub::forward()
+void TeleopPub::forward()
 {
-    this->initMsg();
+    this->init_msg();
     this->msg_.linear.x = 2.0;
-
+    
     RCLCPP_INFO(this->get_logger(), "forward");
     this->publisher_->publish(msg_);
 }
 
-void teleopPub::backward()
+void TeleopPub::backward()
 {
-    this->initMsg();
+    this->init_msg();
     this->msg_.linear.x = -2.0;
 
     RCLCPP_INFO(this->get_logger(), "backward");
     this->publisher_->publish(msg_);
 }
 
-void teleopPub::rotateLeft()
+void TeleopPub::rotateLeft()
 {
-    this->initMsg();
+    this->init_msg();
     this->msg_.angular.z = 2.0;
 
     RCLCPP_INFO(this->get_logger(), "left rotate");
     this->publisher_->publish(msg_);
 }
 
-void teleopPub::rotateRight()
+void TeleopPub::rotateRight()
 {
-    this->initMsg();
+    this->init_msg();
     this->msg_.angular.z = -2.0;
 
     RCLCPP_INFO(this->get_logger(), "right rotate");
     this->publisher_->publish(msg_);
 }
 
-void teleopPub::stop()
+void TeleopPub::stop()
 {
-    this->initMsg();
+    this->init_msg();
 
     RCLCPP_INFO(this->get_logger(), "stop");
     this->publisher_->publish(msg_);
 }
 
-teleopPub::teleopPub()
+TeleopPub::TeleopPub()
     : Node("teleop_pub")
 {
-    this->publisher_ = this->Node::create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
-    this->timer_ = this->create_wall_timer
-        (2000ms, std::bind(&teleopPub::timer_callback, this));
+    this->publisher_ = this->Node::create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    /*this->timer_ = this->create_wall_timer
+        (2000ms, std::bind(&TeleopPub::timer_callback, this));
 
-    this->timer_->cancel();
+    this->timer_->cancel();*/
 }
 
-
-void teleopPub::timer_callback()
+/*
+void TeleopPub::timer_callback()
 {
     geometry_msgs::msg::Twist msg;
 
@@ -83,4 +83,4 @@ void teleopPub::timer_callback()
     msg_.angular.y = 0.0;
     msg_.angular.z = 1.8;
 
-}
+}*/
