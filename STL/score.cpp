7f0a16b6d7738055c2c9ae62e10d0225859f811a
PIUphil <ahnkp08@daum.net>
2021-07-05 11:01:48 +0900

    Update score.cpp

--- STL/score.cpp
+++ STL/score.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <ios>
+#include <iomanip>		// manipulator 조력자, 조작자
 #include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::setprecision;
+using std::streamsize;
 
 int main()
 {
@@ -25,8 +29,14 @@ int main()
 		++count;
 		sum += hw;
 	}
+	
+	streamsize ss = cout.precision();
+	// cout << "Your final score : " << 0.2*midterm + 0.4*finalterm + 0.4*sum/count << endl;
+	cout << "Your final score : " << setprecision(3) 
+		 << 0.2*midterm + 0.4*finalterm + 0.4*sum/count << setprecision(ss) << endl;
 
-	cout << "Your final score : " << 0.2*midterm + 0.4*finalterm + 0.4*sum/count << endl;
+	// setprecision(3) : 숫자 3개만 출력함 -> 정수2자리, 소수1자리 출력됨.. -> 정수1, 소수2자리 출력될수도 있음
+	// setprecision(ss) : 원래 상태로 복구시킴 // streamsize
 
 	return 0;
 }
