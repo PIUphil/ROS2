1fa16ae5ed979886c7d233662bdfc41dcfdb1c9d
PIUphil <ahnkp08@daum.net>
2021-07-19 16:40:10 +0900

    STL_9

---
+++ STL/ScoreGrade11/Student_info.h
@@ -0,0 +1,34 @@
+#ifndef SCOREGRADE9_STUDENT_INFO_H
+#define SCOREGRADE9_STUDENT_INFO_H
+
+//#include <iostream>
+//#include <vector>
+
+#include "Core.h"
+#include "Handle.h"
+
+
+class Student_info {
+public:
+    static bool compare(const Student_info& s1, const Student_info& s2);       // 특정 함수와 관계있는 스태틱멤버함수,,,,;?
+
+    Student_info();
+    Student_info(std::istream& in);
+    //Student_info(const Student_info& rhs);
+    //~Student_info();
+
+    //Student_info& operator=(const Student_info& rhs);
+
+    std::istream& read(std::istream& in);
+
+    std::string name() const;
+    double grade() const;
+
+private:
+    //Core *pCore_;
+    Handle<Core> handle_;
+
+
+};
+
+#endif //SCOREGRADE9_STUDENT_INFO_H
